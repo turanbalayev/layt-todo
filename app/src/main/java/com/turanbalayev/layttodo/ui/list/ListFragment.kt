2cc0144d58c9ba3882adc31893b7c94f33588832
Turan Balayev <turanbalayev96@gmail.com>
2023-07-28 20:09:23 +0400

    Add list fragment menu

--- app/src/main/java/com/turanbalayev/layttodo/ui/list/ListFragment.kt
+++ app/src/main/java/com/turanbalayev/layttodo/ui/list/ListFragment.kt
@@ -3,8 +3,14 @@ package com.turanbalayev.layttodo.ui.list
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.turanbalayev.layttodo.R
 import com.turanbalayev.layttodo.databinding.FragmentListBinding
@@ -19,13 +25,30 @@ class ListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentListBinding.inflate(inflater,container,false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.list_fragment_menu,menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return true
+            }
+
+        },viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+
         binding.floatingActionButton.setOnClickListener {
 
             val action = ListFragmentDirections.actionListFragmentToAddFragment()
@@ -44,6 +67,8 @@ class ListFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+
 
 
 }
