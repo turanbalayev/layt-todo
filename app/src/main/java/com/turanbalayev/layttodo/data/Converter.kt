20dee553b7c89758143e5c75bcf9cafe9707112c
Turan Balayev <turanbalayev96@gmail.com>
2023-07-29 16:36:09 +0400

    Add type coverter to todo database

---
+++ app/src/main/java/com/turanbalayev/layttodo/data/Converter.kt
@@ -0,0 +1,17 @@
+package com.turanbalayev.layttodo.data
+
+import androidx.room.TypeConverter
+
+class Converter {
+
+    @TypeConverter
+    fun fromPriority(priority: Priority): String {
+        return priority.name
+    }
+
+
+    @TypeConverter
+    fun toPriority(priority: String): Priority {
+        return Priority.valueOf(priority)
+    }
+}
