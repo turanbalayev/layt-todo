b9e39b4f7828d213b9d93f9e30d06fe53e6add0e
Turan Balayev <turanbalayev96@gmail.com>
2023-07-29 13:14:28 +0400

    Add add fragment menu

--- app/src/main/java/com/turanbalayev/layttodo/ui/add/AddFragment.kt
+++ app/src/main/java/com/turanbalayev/layttodo/ui/add/AddFragment.kt
@@ -3,8 +3,14 @@ package com.turanbalayev.layttodo.ui.add
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import com.turanbalayev.layttodo.R
 import com.turanbalayev.layttodo.databinding.FragmentAddBinding
 
@@ -21,6 +27,7 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentAddBinding.inflate(inflater,container,false)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -28,6 +35,13 @@ class AddFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.add_fragment_menu,menu)
     }
 
     override fun onDestroyView() {
